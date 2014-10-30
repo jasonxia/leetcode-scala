8fa2e7992fd4c94a6d0bd5623314d983150a594c
Yongmin Xia <yongmin.xia@rea-group.com>
2014-10-30 19:04:23 +1100

    Added solution for reverse words in a string

--- src/main/scala/easy/ReverseInteger.scala
+++ src/main/scala/easy/ReverseInteger.scala
@@ -6,6 +6,13 @@ import scala.annotation.tailrec
  * Created by yongmin_xia on 28/10/2014.
  */
 object ReverseInteger {
-  def reverse(x: Int): Int = ???
+  def reverse(x: Int): Int = {
+    @tailrec
+    def reverseRec(i: Int, acc: Int): Int = i match {
+      case 0 => acc
+      case _ => reverseRec(i/10, acc * 10 + i % 10)
+    }
+    reverseRec(x, 0)
+  }
 
 }
--- src/main/scala/medium/ReverseWordsInAString.scala
+++ src/main/scala/medium/ReverseWordsInAString.scala
@@ -4,5 +4,7 @@ package medium
  * Created by yongmin_xia on 30/10/2014.
  */
 object ReverseWordsInAString {
-  def reverse(s: String): String = ???
+  def reverse(s: String): String = {
+    s.split(" ").filterNot(_.isEmpty).foldLeft(List[String]())((acc, w) => { w :: acc }).mkString(" ")
+  }
 }
