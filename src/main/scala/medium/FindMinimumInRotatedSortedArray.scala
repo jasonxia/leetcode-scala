4c2e90a2ad6f492561eb4f33aa427ff471f85e20
Yongmin Xia <yongmin.xia@rea-group.com>
2014-10-31 19:10:40 +1100

    Added solution to find minimum number in rotated sorted list

--- src/main/scala/medium/FindMinimumInRotatedSortedArray.scala
+++ src/main/scala/medium/FindMinimumInRotatedSortedArray.scala
@@ -1,11 +1,17 @@
 package medium
 
+import scala.annotation.tailrec
+
 /**
  * Created by yongmin_xia on 31/10/2014.
  */
 
 object FindMinimumInRotatedSortedArray {
 
-  def findMin(s: List[Int]): Int = ???
+  @tailrec
+  def findMin(s: List[Int]): Int = s match {
+    case i :: Nil => i
+    case h :: t => if(h < t.last) h else findMin(t)
+  }
 
 }
