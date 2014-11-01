e1ae39d49d01248c4cd2f1f182e20b2390635134
Yongmin Xia <yongmin.xia@rea-group.com>
2014-11-02 01:26:20 +1100

    Added solution to n-queens problem

--- src/main/scala/hard/NQueens.scala
+++ src/main/scala/hard/NQueens.scala
@@ -4,5 +4,23 @@ package hard
  * Created by yongmin_xia on 1/11/2014.
  */
 object NQueens {
-  def queens(n: Int): Set[List[Int]] = ???
+  def queens(n: Int): Set[List[Int]] = {
+    def placeQueens(k: Int): Set[List[Int]] = {
+      if(k == 0) Set(List())
+      else for {
+        queens <- placeQueens(k - 1)
+        col <- 0 until n
+        if isSafe(col, queens)
+      } yield col :: queens
+    }
+    placeQueens(n)
+  }
+
+  def isSafe(col: Int, queens: List[Int]): Boolean = {
+    val rows = queens.length
+    val rowWithQueens = (rows - 1 to 0 by -1) zip queens
+    rowWithQueens forall {
+      case(r, c) => col != c && math.abs(col - c) != rows - r
+    }
+  }
 }
